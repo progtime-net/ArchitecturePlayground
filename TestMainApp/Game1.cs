53d6760b1b32ce1b1d97ded7a7e93e4ccd14a732
SergoDobro <optimgrey@yandex.ru>
2023-08-13 12:19:06 +0300

    initial commit

---
+++ TestMainApp/Game1.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TestMainApp
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        MonogameLibrary.UI.Elements.Slider Slider;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            MonogameLibrary.UI.Base.MonoClassManagerUI.InitManager(GraphicsDevice,
+                Content, "");
+            Slider = new MonogameLibrary.UI.Elements.Slider() { MinValue = 0, MaxValue = 100 };
+            Slider.rectangle = (new Rectangle(0, 0, 400, 30));
+            Slider.SliderChanged += (a) =>
+            {
+                if (a == 100)
+                {
+                    Exit();
+                }
+            };
+            Slider.LoadTexture();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            MonogameLibrary.UI.Base.MonoClassManagerUI.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            MonogameLibrary.UI.Base.MonoClassManagerUI.Draw(_spriteBatch);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ TestMainApp/GameCore/GameObject.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestMainApp.GameCore
+{
+    class Gamemanager
+    {
+        List<GameObject> gameObjects;
+        public void Start()
+        {
+
+        }
+        public void Update()
+        {
+            foreach (var item in animateds)
+                item.UpdateAnimation();
+        }
+        static List<IAnimated> animateds = new List<IAnimated>(); 
+        public static void Register(object Registrable)
+        {
+            if (Registrable is IAnimated) animateds.Add(Registrable as IAnimated);
+        }
+    }
+    abstract class GameObject
+    {
+        public GameObject()
+        {
+            Gamemanager.Register(this);
+        }
+        public abstract void Update();
+    }
+    class LivingEntity : GameObject, IAnimated
+    {
+        public override void Update()
+        {
+
+        }
+        void IAnimated.UpdateAnimation()
+        {
+        }
+
+        void IAnimated.DrawAnimation()
+        {
+        }
+    }
+
+
+
+    class Player : LivingEntity
+    {
+        public override void Update()
+        {
+            
+        }
+        public Player() :base()
+        {
+
+        }
+    }
+
+
+
+    interface IAnimated
+    {
+        void UpdateAnimation();
+        void DrawAnimation();
+    }
+}
